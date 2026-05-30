1327c502bfbafd6449b3571e54bae91b4758096e
EmilP557 <emilpodraza@gmail.com>
2026-05-30 11:35:16 +0200

    LAB04: GET /v1/users - wylistowanie użytkonikow

--- src/main/java/pl/wsb/fitnesstracker/user/internal/UserController.java
+++ src/main/java/pl/wsb/fitnesstracker/user/internal/UserController.java
@@ -1,45 +1,47 @@
 package pl.wsb.fitnesstracker.user.internal;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import pl.wsb.fitnesstracker.user.api.User;
 import pl.wsb.fitnesstracker.user.api.UserDto;
 import pl.wsb.fitnesstracker.user.api.UserProvider;
 import pl.wsb.fitnesstracker.user.api.UserService;
 
 import java.util.List;
 
-/**
- * UserController is responsible for handling HTTP requests related to user operations.
- * It provides endpoints for retrieving and creating users.
- */
 @RestController
 @RequestMapping("/v1/users")
-@RequiredArgsConstructor
 class UserController {
 
     private final UserService userService;
-
     private final UserProvider userProvider;
-
     private final UserMapper userMapper;
+
+    @Autowired
+    UserController(UserService userService, UserProvider userProvider, UserMapper userMapper) {
+        this.userService = userService;
+        this.userProvider = userProvider;
+        this.userMapper = userMapper;
+    }
 
     @PostMapping
     public UserDto addUser(@RequestBody UserDto userDto) throws InterruptedException {
-
-        // TODO: Implement the method to add a new user.
-        //  You can use the @RequestBody annotation to map the request body to the UserDto object.
-
         return null;
     }
 
     @GetMapping
-    public List<UserDto> getUsers() throws InterruptedException {
-
-       return this.userProvider.findAllUsers().stream()
+    @ResponseStatus(HttpStatus.OK)
+    public List<UserDto> getAllUsers() throws InterruptedException {
+        return this.userProvider.findAllUsers().stream()
                 .map(this.userMapper::toUserDto)
                 .toList();
     }
 
-
+    @GetMapping("/simple")
+    @ResponseStatus(HttpStatus.OK)
+    public List<UserDto> getSimpleUsers() throws InterruptedException {
+        return this.userProvider.findAllUsers().stream()
+                .map(this.userMapper::toUserDto)
+                .toList();
+    }
 }
